fa90977bf21827e2d06b7b335d4ca331f8e04fd0
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-05-10 19:00:52 -0300

    feat: create get and post to users

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,33 @@
-const fs = require('fs');
 const router = require('express').Router();
-const path = require('path');
-
-const filePath = path.join(__dirname, '..', 'data', 'users.json');
-
-function readUsersFile(res, callback) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) return res.status(500).json({ message: 'Erro ao ler arquivo' });
-    callback(JSON.parse(data));
-  });
-}
+const users = require('../models/user.js');
 
 router.get('/', (req, res) => {
-  readUsersFile(res, (users) => {
-    res.json(users);
-  });
+  users
+    .find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(500).json({ message: 'User does not exist' }));
 });
 
 router.get('/:id', (req, res) => {
-  readUsersFile(res, (users) => {
-    const user = users.find((user) => user._id === req.params.id);
-    if (!user) return res.status(404).json({ message: 'User not found' });
-    res.send(user);
-  });
+  const { id } = req.params;
+
+  users
+    .findById(id)
+    .then((user) => {
+      if (user === null) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    })
+    .catch((err) => res.status(500).json({ message: 'User does not exist' }));
+});
+
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body;
+  users
+    .create({ name, about, avatar })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json({ message: 'Something went wrong' }));
 });
 
 module.exports = router;
